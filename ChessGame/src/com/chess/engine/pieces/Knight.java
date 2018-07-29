47723bf4a54b9b8c7ca21b873148322df0ff4bdc
Sam Florin <sam.florin@hotmail.com>
2018-07-30 00:50:05 +0200

    Added the King and updated the Board

--- ChessGame/src/com/chess/engine/pieces/Knight.java
+++ ChessGame/src/com/chess/engine/pieces/Knight.java
@@ -17,7 +17,7 @@ public class Knight extends Piece {
 
     private final static int[] CANDIDATE_MOVE_COORDINATES = {-17, -15, -10, -6, 6, 10, 15, 17};
 
-    Knight(final int piecePosition, final Alliance pieceAlliance) {
+    public Knight(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
@@ -56,7 +56,7 @@ public class Knight extends Piece {
     }
 
     private static boolean isSecondColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.SECOND_COLUMN[currentPosition] && ((candidateOffset == -10) || candidateOffset == -6);
+        return BoardUtils.SECOND_COLUMN[currentPosition] && ((candidateOffset == -10) || candidateOffset == 6);
     }
 
     private static boolean isSeventhColumnExclusion(final int currentPosition, final int candidateOffset) {
