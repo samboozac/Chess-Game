00b4708b6a2ef96e12a73bd56e3cc6585d2dfa23
Sam Florin <sam.florin@hotmail.com>
2018-07-30 02:22:09 +0200

    Added a gitignore file and updated the code

--- ChessGame/src/com/chess/engine/JChess.java
+++ ChessGame/src/com/chess/engine/JChess.java
@@ -3,6 +3,7 @@ package com.chess.engine;
 import com.chess.engine.board.Board;
 
 public class JChess {
+
     public static void main(String[] args) {
         Board board = Board.createStandardBoard();
 
